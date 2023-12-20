3464ea96be93cf0df848a1b5e82f66b4563fbaca
vitodursopettersson <vitodurso98@gmail.com>
2023-12-20 12:17:20 +0100

    new dependencies fontawesome

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import './assets/styles/general.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+library.add(faChevronDown)
