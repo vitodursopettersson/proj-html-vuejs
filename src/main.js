87c2bf136741385be9b1228bb38a41c9d93c53bb
vitodursopettersson <vitodurso98@gmail.com>
2023-12-20 12:21:12 +0100

    fix fontawesome

--- src/main.js
+++ src/main.js
@@ -9,3 +9,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 library.add(faChevronDown)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
