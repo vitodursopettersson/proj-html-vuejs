04c8836a0ab82b4b3e88c5c48aa2c98b4f38c20d
vitodursopettersson <vitodurso98@gmail.com>
2023-12-20 14:33:47 +0100

    import icon section above

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import './assets/styles/general.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
-
 // FontAwesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -11,7 +9,8 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-library.add(faChevronDown, faCartShopping, faCircleUser, faMagnifyingGlass)
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+library.add(faChevronDown, faCartShopping, faCircleUser, faMagnifyingGlass, faDownload)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
