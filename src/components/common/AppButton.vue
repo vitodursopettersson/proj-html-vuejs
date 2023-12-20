97b7b809c9c1f3c21084e93dbaf717ed8b6c1233
vitodursopettersson <vitodurso98@gmail.com>
2023-12-20 14:34:29 +0100

    common button component

---
+++ src/components/common/AppButton.vue
@@ -0,0 +1,48 @@
+<script>
+
+export default {
+    name: 'AppButton'
+}
+
+</script>
+
+<template>
+    <button>
+        <div class="icon">
+            <slot name="icon"></slot>
+        </div>
+        <div class="text">
+            <slot name="text"></slot>
+        </div>
+    </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+    background-color: #4BAF97;
+    width: fit-content;
+    padding: 10px 32px;
+    display: flex;
+    gap: 14px;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+
+    &:hover {
+        background-color: #469f8a;
+        transition: background-color 0.1s ease-in-out;
+    }
+
+    .icon {
+        font-size: 18px;
+    }
+
+    .text {
+        font-size: 12px;
+        font-weight: 500;
+    }
+
+}
+</style>
